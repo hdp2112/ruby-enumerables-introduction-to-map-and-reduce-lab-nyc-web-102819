3a513b05f7c8df7796b78eb2279a955bbb448715
Hector Polanco <hdp2112@columbia.edu>
2019-10-20 01:18:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,7 +35,6 @@ array.length.times do |index|
 end  
 
 def reduce_to_total(source_array, starting_point=0)
-  starting_point = 100
   new = starting_point
   i = 0
   while i < source_array.length do
